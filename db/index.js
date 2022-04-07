d1b36a518567827f00155b0766e766b395c855c3
omokhele <anne.imomoh@gmail.com>
2022-04-07 12:34:58 -0700

    add any pending changes

--- db/index.js
+++ db/index.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
-// dotenv.config();
+const dotenv = require("dotenv");
+dotenv.config();
 
 
-let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/polar-meadow-67976'
+// let MONGODB_URI = process.env.PROD_MONGODB || process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/NewItemsDataBase'
 
-mongoose.connect(MONGODB_URI, {useUnifiedTopology: true, useNewUrlParser: true}).then(()=> {
+mongoose.connect(process.env.MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true}).then(()=> {
     console.log('Successfully connected to MongoDB')
 }).catch(e=> {
     console.error('Connection error', e.message)
